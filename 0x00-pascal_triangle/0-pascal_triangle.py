abf2c246cff078edcc18a22254530e3b46ae112e
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-07 02:52:24 +0300

    Fix code error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
         return []
 
     triangle = []
-    
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
+
         triangle.append(row)
 
     return triangle
