64e8502d615e8d3bb358da01bf85d32cfa14f165
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-20 01:46:25 +0300

    Minimal operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Minimal operations"""
+
+
+def minOperations(n):
+    """Operations"""
+    if n == 1:
+        return 0
+
+    operations = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    return operations if n == 1 else 0
