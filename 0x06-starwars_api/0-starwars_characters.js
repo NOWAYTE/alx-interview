d17318c550ec420c7ec5028740893f07be472382
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-25 20:53:45 +0300

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,34 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
 
-const request = require('request');
+const axios = require('axios');
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
+async function fetchMovieCharacters(movieId) {
+    const baseUrl = "https://swapi.dev/api/films/";
 
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
+    try {
+        // Fetch movie details
+        const movieResponse = await axios.get(`${baseUrl}${movieId}/`);
+        const movieData = movieResponse.data;
+
+        // Get the list of character URLs
+        const characters = movieData.characters;
+
+        // Fetch and print each character name
+        for (const characterUrl of characters) {
+            const charResponse = await axios.get(characterUrl);
+            console.log(charResponse.data.name);
+        }
+    } catch (error) {
+        console.error(`Error fetching data: ${error.message}`);
+        process.exit(1);
     }
-  } else {
-    console.log(error);
-  }
-});
-
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-  request(url, function (error, response, body) {
-    if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
-    } else {
-      console.error('error:', error);
-    }
-  });
 }
+
+if (process.argv.length !== 3) {
+    console.error("Usage: node star_wars_characters.js <Movie ID>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+fetchMovieCharacters(movieId);
+
