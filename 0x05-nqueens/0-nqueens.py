3ae342c089755cd1ce4d9c8640484a322168607f
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-25 20:33:59 +0300

    Add README.md

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,36 +1,54 @@
-Tasks
-0. N queens
-mandatory
-Score: 0.0% (Checks completed: 0.0%)
+#!/usr/bin/python3
+"""
+N queens
+"""
 
-Chess grandmaster Judit Polgár, the strongest female chess player of all time
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
-The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
 
-Usage: nqueens N
-If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
-where N must be an integer greater or equal to 4
-If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
-If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
-The program should print every possible solution to the problem
-One solution per line
-Format: see example
-You don’t have to print the solutions in a specific order
-You are only allowed to import the sys module
-Read: Queen, Backtracking
 
-julien@ubuntu:~/0x08. N Queens$ ./0-nqueens.py 4
-[[0, 1], [1, 3], [2, 0], [3, 2]]
-[[0, 2], [1, 0], [2, 3], [3, 1]]
-julien@ubuntu:~/0x08. N Queens$ ./0-nqueens.py 6
-[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]
-[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]
-[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]
-[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]
-julien@ubuntu:~/0x08. N Queens$ 
-Repo:
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
 
-GitHub repository: alx-interview
-Directory: 0x05-nqueens
-File: 0-nqueens.py
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
+            return False
+    return True
+
+
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
